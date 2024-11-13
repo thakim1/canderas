689211387aca53d2d9f5c68a5a99f993f7f57751
fabianseiler <f.seiler.privat@gmail.com>
2024-11-13 12:30:49 +0100

    Upload Training Script + Models

---
+++ Training/Nordland_dataloader.py
@@ -0,0 +1,128 @@
+"""
+author: Fabian Seiler @ 20.06.24
+"""
+import os
+from torchvision import datasets, transforms
+import torchvision.transforms.functional as F
+from torchvision.datasets import VisionDataset
+import torch
+import cv2
+from tqdm import tqdm
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# Loads Test/Training Data into one Data folder
+
+class Nordland():
+
+    def __init__(self, batch_size, root="/srv/harmony/Data/Nordland/Segmentation_new", augment: bool = False):
+
+        self.root = root
+        self.batch_size = batch_size
+
+        self.T = transforms.Compose([
+            transforms.ToPILImage(),
+            transforms.ToTensor(),
+        ])
+
+        self.data = self.load_data(augment)
+
+    def __getitem__(self, index: int):
+        return self.data[index]
+    
+    def __len__(self):
+        
+        return len(self.data)
+
+        total_samples = 0
+        for batch in self.data:
+            
+            total_samples += batch[0].size(0)  # batch[0] is the images tensor
+        return total_samples
+
+    def crop_img(self, img, h_line, v_delta):
+        #img = cv2.cvtColor(img, cv2.COLOR_BRG2RGB)
+        if len(img.shape) == 3:
+            h, w, c = img.shape
+        else:
+            h, w = img.shape
+        left = (w - 2*v_delta) // 2
+        top = h - h_line
+        img = self.T(img)
+        return F.crop(img, top, left, h_line, 2*v_delta)
+
+    def mirror_imgs(self, img):
+        return torch.flip(img, [2])
+    
+    def change_lighting(self, img):
+
+        transform = transforms.ColorJitter(
+            brightness=0.2,  # Change brightness by up to 20%
+            saturation=0.2  # Change saturation by up to 20%
+        )
+        # Apply the transform only to the image
+        img_aug = transform(img)
+        return img_aug
+
+    def load_data(self, augmentation: bool = False):
+        """ Load all training images"""
+        train_imgs = []
+        train_anomaly = []
+        #train_path = os.path.join(self.root, "train/")
+        train_path = os.path.join(self.root, "train/")
+        
+        # Load all training data into one container
+        for folder in os.listdir(train_path):
+            if folder != 'summer_test': continue
+            for subfolder in os.listdir(train_path + folder):
+                print(f"folder={folder}_{subfolder[-1]}")
+                #if subfolder != 'section1': continue
+                for i, image in enumerate(tqdm(os.listdir(train_path + folder + "/" + subfolder))):
+                    if image.endswith('.png'):
+                        # Crop and append input image
+                        #cropped_img = self.crop_img(cv2.imread(train_path + folder + "/" + subfolder + "/" + image), h_line=512, v_delta=480)
+                        cropped_img = self.T(cv2.imread(train_path + folder + "/" + subfolder + "/" + image))
+                        train_imgs.append(cropped_img)
+                        anom = image.split('.png')[0].startswith('composite')
+
+                        if anom:
+                            train_anomaly.append(True)
+                        else:
+                            train_anomaly.append(False)
+
+                        
+                        if augmentation:
+                            # Flip around x-axis (left - right)
+                            img_flip = self.mirror_imgs(cropped_img)
+                            train_imgs.append(img_flip)
+    
+                            if anom:
+                                train_anomaly.append(True)
+                            else:
+                                train_anomaly.append(False)
+
+                            # Lighting effects
+                            for _ in range(3):
+                                img_l = self.change_lighting(cropped_img)
+                                train_imgs.append(img_l)
+                                if anom:
+                                    train_anomaly.append(True)
+                                else:
+                                    train_anomaly.append(False)
+                        
+        # Stack the images + ground truths
+        train_normal_images = torch.stack(train_imgs)
+        return list(zip(train_anomaly, train_normal_images))
+
+
+if __name__ == '__main__':
+    dataset = Nordland(batch_size=1, root="/srv/harmony/Data/Nordland/Binary_classification", augment=False)
+    #for i in dataset.train_loader:
+        #print(i)
+    for data in dataset:
+        anom, img = data
+        #print(img.shape)
+        print(anom)
+        print(img.shape)
+
---
+++ Training/evaluate_model.py
@@ -0,0 +1,220 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+from einops import rearrange, repeat
+import torch.utils
+from torchsummary import summary
+from Nordland_dataloader import Nordland
+import numpy as np
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+import timm
+import torchvision.transforms as transforms
+import torchvision
+from torchvision import datasets
+from PIL import Image
+import cv2
+from torch.utils.data import Dataset, DataLoader, random_split
+import pandas as pd
+from sklearn.metrics import accuracy_score, precision_score, recall_score
+import torch.nn.utils.prune as prune
+import copy
+
+
+def get_transform(img_size=224, normalize=([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])):
+    transform = transforms.Compose([
+            transforms.Resize((img_size, img_size)), 
+            transforms.Normalize(mean=normalize[0], std=normalize[1]) # Normalize with ImageNet stats
+            ])
+    return transform
+
+
+def global_prune_model(model, prune_method, amount=0.3):
+    """
+    Applies global pruning to a copy of the given model and returns the pruned model.
+    
+    Parameters:
+    model (torch.nn.Module): The original trained PyTorch model.
+    amount (float): The proportion of weights to prune globally. Default is 0.3.
+    
+    Returns:
+    torch.nn.Module: The pruned model with global pruning applied.
+    """
+    pruned_model = copy.deepcopy(model)
+    parameters_to_prune = []
+    
+    # Collect all parameters to be pruned globally
+    for name, module in pruned_model.named_modules():
+        if isinstance(module, torch.nn.Conv2d) or isinstance(module, torch.nn.Linear):
+            # Add (module, 'weight') tuples for pruning
+            parameters_to_prune.append((module, 'weight'))
+    
+    if prune_method == 'L1-Unstructured':
+        # L1 Unstructured Pruning
+        prune.global_unstructured(
+        parameters_to_prune,
+        pruning_method=prune.L1Unstructured,
+        amount=0.3
+        )
+    elif prune_method == 'Random':
+        # Random Unstructured Pruning
+        prune.global_unstructured(
+        parameters_to_prune,
+        pruning_method=prune.RandomUnstructured,
+        amount=0.3
+        )
+
+    elif prune_method == 'L2-Structured':
+        # L2 Structured Pruning
+        prune.global_unstructured(
+        parameters_to_prune,
+        pruning_method=lambda module, name, amount: prune.LnStructured(module, name, amount=amount, n=2, dim=0),
+        amount=0.3
+        )
+    
+    return pruned_model
+
+
+
+if __name__ == '__main__':
+
+    # Parameters:
+    lr = 3e-4
+    max_epoch = 20
+    batch_size = 32
+    gamma=0.1
+    weights = 'IMAGENET1K_V1'
+
+    # DenseNet_121
+    # MobileNet_v2
+    # MobileNet_v3_S
+    # ResNet18
+    # EfficientNet_v2_S
+    # MobileNet_v3_B
+    
+    # Load Dataset and Create Train/Test Split:
+    dataset = Nordland(batch_size=batch_size, root="/srv/harmony/Data/Nordland/AnomDataset", augment=False)
+    train_size = int(0.8 * len(dataset))
+    test_size = len(dataset) - train_size
+    train_dataset, test_dataset = random_split(dataset, [train_size, test_size])
+    data_loader_train = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    data_loader_test = DataLoader(test_dataset, batch_size=batch_size, shuffle=False) 
+
+    results = []
+
+    model_names = ['DenseNet', 'MobileNetV2', 'MobileNetV3', 'ResNet18', 'EfficientNet', 'MobileNetV3_big']
+
+    for model_name in model_names:
+        print(f"-------------{model_name}-------------")
+    
+        # Load the model and change classification layer
+        if model_name == 'DenseNet':
+            model = torchvision.models.densenet121(weights=weights)
+            model.classifier = nn.Sequential(nn.Linear(in_features=model.classifier.in_features, out_features=1), nn.Sigmoid())
+
+        elif model_name == 'MobileNetV2': 
+            model = torchvision.models.mobilenet_v2(weights=weights)
+            model.classifier[1] = nn.Sequential(nn.Linear(in_features=1280, out_features=1), nn.Sigmoid())
+
+        elif model_name == 'MobileNetV3':
+            model = torchvision.models.mobilenet_v3_small(weights=weights)
+            model.classifier[3] = nn.Sequential(nn.Linear(in_features=1024, out_features=1), nn.Sigmoid())
+
+        elif model_name == 'ResNet18':
+            model = torchvision.models.resnet18(weights=weights)
+            model.fc = nn.Sequential(nn.Linear(model.fc.in_features, 1), nn.Sigmoid())
+
+        elif model_name == 'EfficientNet':
+            model = torchvision.models.efficientnet_v2_s(weights=weights)
+            model.classifier[1] = nn.Sequential(nn.Linear(1280, 1), nn.Sigmoid())
+
+        elif model_name == 'MobileNetV3_big':
+            model = torchvision.models.mobilenet_v3_large(weights=weights)
+            model.classifier[3] = nn.Sequential(nn.Linear(in_features=1280, out_features=1), nn.Sigmoid())
+        
+
+        # Move Model to GPU and show Layer
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        print(device)
+        model.to(device)
+        #print(model)
+
+        # Setup for Loss/Optimizer/Scheduler
+        T = get_transform()
+        criterion = nn.BCELoss() #nn.CrossEntropyLoss()
+        optimizer = torch.optim.Adam(model.parameters(), lr=lr)  # Adam optimizer
+        scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=gamma)
+
+        # Start Training:
+        print("Training Started!\n")
+        model.train()
+        for epoch in range(max_epoch):
+            
+            TP = FP = TN = FN = 0
+
+            for sample in tqdm(data_loader_train):
+                # Load Image and Label + Preprocessing
+                anomaly, img = sample
+                img_crop = T(img.view(img.shape[0], 3, img.shape[2], img.shape[3]))
+                target_tensor = anomaly.to(torch.float32).to(device).view((-1,1))
+
+                # Forward Pass + Backprop
+                result = model((img_crop).to(device))
+                loss = criterion(result, target_tensor)
+                #optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+                scheduler.step()
+
+                # Evaluate the Confusion Matrix
+                predictions = (result >= 0.5).float()  # Binary prediction threshold
+                TP += ((predictions == 1) & (target_tensor == 1)).sum().item()
+                TN += ((predictions == 0) & (target_tensor == 0)).sum().item()
+                FP += ((predictions == 1) & (target_tensor == 0)).sum().item()
+                FN += ((predictions == 0) & (target_tensor == 1)).sum().item()
+
+            # Calculate Metrics after each Epoch
+            train_accuracy = (TP + TN) / (TP + TN + FP + FN) if (TP + TN + FP + FN) > 0 else 0
+            train_precision = TP / (TP + FP) if (TP + FP) > 0 else 0
+            train_recall = TP / (TP + FN) if (TP + FN) > 0 else 0
+            train_false_positive_rate = FP / (FP + TN) if (FP + TN) > 0 else 0
+
+            #---------------------------------------------------------------------------------
+            # Evaluate on Test Dataset
+            TP = FP = TN = FN = 0
+
+            with torch.no_grad():
+                for sample in tqdm(data_loader_test):
+                    #Load Image and Label + Preprocessing
+                    anomaly, img = sample
+                    img_crop = T(img.view(img.shape[0], 3, img.shape[2], img.shape[3]))
+                    target_tensor = anomaly.to(torch.float32).to(device).view((-1,1))
+
+                    # Forward Pass 
+                    result = model((img_crop).to(device))
+                    loss_t = criterion(result, target_tensor)
+
+                    # Evaluate the Confusion Matrix
+                    predictions = (result >= 0.5).float()  # Binary prediction threshold
+                    TP += ((predictions == 1) & (target_tensor == 1)).sum().item()
+                    TN += ((predictions == 0) & (target_tensor == 0)).sum().item()
+                    FP += ((predictions == 1) & (target_tensor == 0)).sum().item()
+                    FN += ((predictions == 0) & (target_tensor == 1)).sum().item()
+
+            # Calculate Metrics after each Epoch
+            test_accuracy = (TP + TN) / (TP + TN + FP + FN) if (TP + TN + FP + FN) > 0 else 0
+            test_precision = TP / (TP + FP) if (TP + FP) > 0 else 0
+            test_recall = TP / (TP + FN) if (TP + FN) > 0 else 0
+            test_false_positive_rate = FP / (FP + TN) if (FP + TN) > 0 else 0      
+
+            print(f"Epoch {epoch+1}, TRAIN || Accuracy: {train_accuracy:.4f}, Precision: {train_precision:.4f}, Recall: {train_recall:.4f}, False Positive Rate: {train_false_positive_rate:.4f}")
+            print(f"Epoch {epoch+1}, TEST  || Accuracy: {test_accuracy:.4f}, Precision: {test_precision:.4f}, Recall: {test_recall:.4f}, False Positive Rate: {test_false_positive_rate:.4f}")
+            
+        s_tr = f"TRAIN || A={train_accuracy:.4f} | P={train_precision:.4f} | R={train_recall:.4f} | FPR={train_false_positive_rate:.4f}\n"
+        s_te = f"TEST || A={test_accuracy:.4f} | P={test_precision:.4f} | R={test_recall:.4f} | FPR={test_false_positive_rate:.4f}"
+        results.append([s_tr + s_te])
+
+        modelpath = "./SoC_Labor/Models/model_{}_epochs_{}_lr_{}_gammaStepLR_{}".format(model_name, max_epoch, lr, gamma)
+        torch.save(model.state_dict(), modelpath)
+
+    print([res for res in results])
